6d86a89cae4dc02f657ed03a864abf23ab46cd96
Mikkel-KR <Mikkel.kora@gmail.com>
2020-03-27 13:09:04 +0100

    Implemented TextSaver and ITextSaver

---
+++ Memento_Classes/TextSaver.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento_Classes
+{
+    public interface ITextSaver
+    {
+        void HandleUndo();
+        void HandleRedo();
+        void HandleSaveMemento();
+    }
+
+    public class TextSaver : ITextSaver
+    {
+        //Fields
+        IOriginator _textOriginator;
+        List<object> _undoList;
+        List<object> _redoList;
+
+        public TextSaver()
+        {
+            _textOriginator = new TextEditor();
+            _undoList = new List<object>();
+            _redoList = new List<object>();
+        }
+
+        public void HandleUndo()
+        {
+            if(_undoList.Count > 0)
+            {
+                // Create new memento from current state and add to redoList
+                var newMemento = _textOriginator.CreateState();
+                _redoList.Add(newMemento);
+
+                // Get newest undo-memento, restore the state and delete from list
+                var mementoToRestore = _undoList.Last();
+                _textOriginator.RestoreState(mementoToRestore);
+                _undoList.Remove(mementoToRestore);
+            }
+        }
+
+        public void HandleRedo()
+        {
+            if (_undoList.Count > 0)
+            {
+                // Create new memento from current state and add to redoList
+                var newMemento = _textOriginator.CreateState();
+                _undoList.Add(newMemento);
+
+                // Get newest redo-memento, restore the state and delete from list
+                var mementoToRestore = _redoList.Last();
+                _textOriginator.RestoreState(mementoToRestore);
+                _undoList.Remove(mementoToRestore);
+            }
+        }
+
+        public void HandleSaveMemento()
+        {
+            // Check : Current state for PageTextContent != the newest saved change
+            if(_textOriginator.CreateState() != _undoList.Last())
+            {
+                var newMemento = _textOriginator.CreateState();
+                _undoList.Add(newMemento);
+            }
+        }
+    }
+}
